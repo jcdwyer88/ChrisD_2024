1bdf0132f6ab439cdfddefe8e32e321112349d03
Jonathan Dwyer <jonathan.c.dwyer.mil@army.mil>
2024-09-03 10:52:33 -0500

    more demo

--- ObjectsDemo/scripts.js
+++ ObjectsDemo/scripts.js
@@ -15,7 +15,22 @@ let person = { // Creates object for array with specified values/types
     hobbies: "Cooking",
 }
 
-console.log(person.fullName()) // Dot notation to reference 'fullName' from object 'person'
-console.log(person.family[2]) // Dot notation to find index 2 value in family
-console.log(person["family"][2]) // Bracket notation for the same
-console.log(person["associates"]["friend"])
+// console.log(person.fullName()) // Dot notation to reference 'fullName' from object 'person'
+// console.log(person.family[2]) // Dot notation to find index 2 value in family
+// console.log(person["family"][2]) // Bracket notation for the same
+// console.log(person["associates"]["friend"])
+
+
+// day 2
+// destructuring
+
+// let fname = person.fname
+// let lname = person.lname
+// let age = person.age
+// console.log(`${fname} ${lname} is ${age} years old`)
+
+// orrrrr
+let {fname, lname, age, family} = person
+console.log(`${fname} ${lname} is ${age} years old`)
+console.log(family.length)
+
