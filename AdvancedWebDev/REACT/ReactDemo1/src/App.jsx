fec65c3b4314fe1e8e26250c4bb52abb4c299627
Jonathan Dwyer <jonathan.c.dwyer.mil@army.mil>
2024-09-24 11:54:13 -0500

    react demo 1

---
+++ AdvancedWebDev/REACT/ReactDemo1/src/App.jsx
@@ -0,0 +1,19 @@
+// foundation
+import Child from "./components/child";
+
+const App = () => {
+  let children = ["Johnny", "Jamie", "Jessie", "Caleb", "Zack"]
+  children = children.sort().reverse();
+  let newArray = children.map((kid, index) => {
+    return <Child key={index} fname={kid}/>
+  })
+  return (
+    // fragment with no default formatting <>
+    <> 
+      <h1>My kids are: </h1>
+      {newArray}
+    </>
+  )
+}
+
+export default App;
