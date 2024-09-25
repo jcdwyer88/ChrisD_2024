26cbf59f595ae62cde19cbf917618b0387998131
Jonathan Dwyer <jonathan.c.dwyer.mil@army.mil>
2024-09-25 10:15:37 -0500

    counter-app demo

---
+++ AdvancedWebDev/REACT/counter-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [days, setDays] = useState(303);
+
+  return (
+    <div className="App">
+      <h1>Birthday Party Count Down App</h1>
+      <div className="card">
+        <button onClick={() => setDays(days => days - 1)}>
+          Days remaining until Jon's Birthday are: {days}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
