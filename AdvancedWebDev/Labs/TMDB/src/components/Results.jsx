74a5d46817ed96cd9544b4eceee8e4e3a0135be0
Jonathan Dwyer <jonathan.c.dwyer.mil@army.mil>
2024-10-15 08:30:56 -0500

    updated

--- AdvancedWebDev/Labs/TMDB/src/components/Results.jsx
+++ AdvancedWebDev/Labs/TMDB/src/components/Results.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 import { useLocation } from "react-router-dom";
 
 const Results = () => {
-    const { VITE_TMDB_API_TOKEN } = process.env;
+    const token = process.env.REACT_APP_VITE_TMDB_TOKEN;
     const location = useLocation();
     const searchTerm = location.state?.searchTerm || ""; // Access the search term from location state
     const [movies, setMovies] = useState([]);
@@ -20,7 +20,7 @@ const Results = () => {
               `https://api.themoviedb.org/3/movie/now_playing`,
               {
                 params: {
-                  api_key: VITE_TMDB_API_TOKEN,
+                  api_key: token,
                   language: "en-US",
                   page: 1,
                 },
@@ -31,7 +31,7 @@ const Results = () => {
               {
                 params: {
                   query: searchTerm,
-                  api_key: VITE_TMDB_API_TOKEN,
+                  api_key: token,
                   include_adult: "false",
                   language: "en-US",
                   page: 1,
@@ -49,7 +49,7 @@ const Results = () => {
     };
 
     fetchMovies();
-  }, [searchTerm, VITE_TMDB_API_TOKEN]); // Dependency array includes searchTerm
+  }, [searchTerm, token]); // Dependency array includes searchTerm
 
   if (loading) return <div>Loading...</div>
   if (error) return <div>{error}</div>;
--- AdvancedWebDev/Labs/TMDB/vite.config.js
+++ AdvancedWebDev/Labs/TMDB/vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': dotenv.config().parsed
+    'process.env': {
+     ...process.env 
+    }
   }
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': dotenv.config().parsed
+//   }
+// })
